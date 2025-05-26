74148ed1f66549b083849dca1a2eae91b7178975
conert mavuso <conertmavuso@gmail.com>
2025-05-26 17:43:02 +0200

    selecting word from object clean

--- src/initialize_game.js
+++ src/initialize_game.js
@@ -1,72 +1,39 @@
 const { Hangman } = require("./play");
 const wordToGuess = require("../words.json");
 
-let globalTheme 
-const objectName = {
-  // "tv_entertainment": tv_entertainment,
-  "otherr": other
-}
+let globalTheme;
 
-function chosenWord(theme) {
-  // themeSelector()
-  //choose topic
-  // console.log("here      "+ theme)
-  // let guess = Math.floor(Math.random() * Object.keys(wordToGuess.technology).length)
-  // console.log("number of items  =  " +Object.keys(wordToGuess.technology).length)
-  // console.log("guess       " + guess)
-
-  //choose word from the topic
-  // let wordGuessNumber = Math.floor(Math.random() * Object.keys(wordToGuess.technology).length) 
-  // let actualWord = 
-  // let tt = theme
-  // console.log("---------------------------------   "+wordToGuess)
-  // return wordToGuess.technology.techCompanies[guess];
-}
-function themeSelector(theme){
-
-
-  // let selected_theme = wordToGuess[theme]
+function themeSelector(theme) {
   const keys = Object.keys(wordToGuess[theme]);
   const randomKey = keys[Math.floor(Math.random() * keys.length)];
-    console.log("here   -----------   " + randomKey)
-  globalTheme = randomKey
-  let guess = Math.floor(Math.random() * Object.keys(wordToGuess[theme][randomKey]).length)
-  console.log(wordToGuess[theme][randomKey][guess])
-
-  return wordToGuess[theme][randomKey][guess]
-
+  globalTheme = randomKey;
+  let guess = Math.floor(
+    Math.random() * Object.keys(wordToGuess[theme][randomKey]).length
+  );
+  
+  return wordToGuess[theme][randomKey][guess];
 }
 
 function initializePlay(selectedTheme) {
   let number = [];
   for (let i = 0; i < selectedTheme.length; i++) {
-    // if there is a space then replace the "_" with an empty space
-    // to see win check if there is an "_" in the game if not then it is a WIN!!! 
-    if(selectedTheme[i] == " "){
-      number.push(" ")
-    }else{
+    if (selectedTheme[i] == " ") {
+      number.push(" ");
+    } else {
       number.push("_");
     }
   }
   document.getElementById("play-area").innerHTML = number.join("");
+  document.getElementById("global-theme").innerHTML = globalTheme;
   return number;
 }
 
-
-// initializePlay();
-// let hang = new Hangman(chosenWord(), initializePlay());
-
-// chosenWord(theme);
-
-document.querySelectorAll("#theme button").forEach(button => {
+document.querySelectorAll("#theme button").forEach((button) => {
   button.addEventListener("click", () => {
-    const theme = button.id; // e.g., "technology"
-    // console.log(theme)
+    const theme = button.id;
     const selectedTheme = themeSelector(theme);
-    // chosenWord(selectedTheme)
 
-    // const { word, display } = initializePlay(selectedTheme);
-    const display = initializePlay(selectedTheme)
+    const display = initializePlay(selectedTheme);
     let hang = new Hangman(selectedTheme, display, globalTheme);
 
     // Optionally store hang for future gameplay input
@@ -74,4 +41,4 @@ document.querySelectorAll("#theme button").forEach(button => {
   });
 });
 
-module.exports = { initializePlay};
+module.exports = { initializePlay };
--- src/play.js
+++ src/play.js
@@ -53,22 +53,3 @@ class Hangman {
 }
 
 module.exports = { Hangman };
-
-// let ran = Math.floor(Math.random() * 50)
-// console.log(ran)
-// console.log(data.movies[ran])
-// console.log(data.techCompanies[ran])
-// console.log(data.ProgrammingLanguages[ran])
-// console.log(data.mythicalCreatures[ran])
-// console.log(data.cartoonCharacters[ran])
-// console.log(data.funActivities[ran])
-// console.log(data.produce[ran])
-// console.log(data.animals[ran])
-// console.log(data.periodicTableElements[ran])
-// console.log(data.planets[ran])
-// console.log(data.body_parts[ran])
-// console.log(data.scientists[ran])
-// console.log(data.countries[ran])
-// console.log(data.cities[ran])
-// console.log(data.tv_shows[ran])
-// console.log(data.famous_people[ran])
